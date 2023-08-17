0ff3c551e47af526fcb745bc4ec4fc9f547e8b42
Tugbay Atilla <tugbayatilla@gmail.com>
2023-08-17 12:37:54 +0200

    refactor: use IStrategy instead of Strategy abstract class in CharStrategy

--- Zealot.TestDataBuilder/Strategies/CharStrategy.cs
+++ Zealot.TestDataBuilder/Strategies/CharStrategy.cs
@@ -1,17 +1,21 @@
+using System.Linq.Expressions;
 using Zealot.Interfaces;
 
 namespace Zealot.Strategies;
 
-internal class CharStrategy : Strategy
+internal class CharStrategy : IStrategy
 {
     private const char A = 'A';
 
-    public override IEnumerable<Type> AvailableTypes => new[]
+    public IEnumerable<Type> AvailableTypes => new[]
     {
         typeof(char?), typeof(char)
     };
 
-    public override object Execute(IContext context)
+    public Expression<Func<Type, bool>> ResolveCondition 
+        => info => AvailableTypes.Any(x=>x == info);
+
+    public object Execute(IContext context)
     {
         return A;
     }
