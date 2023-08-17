973fb4611dfc81e74b7a425ffb56041b0593cdba
Tugbay Atilla <tugbayatilla@gmail.com>
2023-08-17 12:40:48 +0200

    refactor: use IStrategy instead of Strategy abstract class in DatetimeStrategy

--- Zealot.TestDataBuilder/Strategies/DatetimeStrategy.cs
+++ Zealot.TestDataBuilder/Strategies/DatetimeStrategy.cs
@@ -1,16 +1,20 @@
-﻿using Zealot.Interfaces;
+﻿using System.Linq.Expressions;
+using Zealot.Interfaces;
 
 namespace Zealot.Strategies;
 
-internal class DatetimeStrategy : Strategy
+internal class DatetimeStrategy : IStrategy
 {
-    public override IEnumerable<Type> AvailableTypes => new[]
+    public IEnumerable<Type> AvailableTypes => new[]
     {
         typeof(DateTime),
         typeof(DateTime?)
     };
 
-    public override object Execute(IContext context)
+    public Expression<Func<Type, bool>> ResolveCondition 
+        => info => AvailableTypes.Any(x=>x == info);
+
+    public object Execute(IContext context)
     {
         return context.With.Date.UtcDate;
     }
