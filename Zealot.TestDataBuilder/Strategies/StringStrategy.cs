b541824dc007af2774dcd199948e37f5077fae5d
Tugbay Atilla <tugbayatilla@gmail.com>
2023-08-17 12:47:31 +0200

    refactor: use IStrategy instead of Strategy abstract class in StringStrategy

--- Zealot.TestDataBuilder/Strategies/StringStrategy.cs
+++ Zealot.TestDataBuilder/Strategies/StringStrategy.cs
@@ -1,13 +1,16 @@
-﻿using Zealot.Interfaces;
+﻿using System.Linq.Expressions;
+using Zealot.Interfaces;
 
 namespace Zealot.Strategies;
 
-internal class StringStrategy : Strategy
+internal class StringStrategy : IStrategy
 {
     private int _number;
-    public override IEnumerable<Type> AvailableTypes => new[] { typeof(string) };
+    public IEnumerable<Type> AvailableTypes => new[] { typeof(string) };
+    public Expression<Func<Type, bool>> ResolveCondition 
+        => info => AvailableTypes.Any(x=>x == info);
 
-    public override object Execute(IContext context)
+    public object Execute(IContext context)
     {
         _number++;
         return $"{context.With.String.Prefix}{context.Scope.PropertyName}_{_number}{context.With.String.Suffix}";
