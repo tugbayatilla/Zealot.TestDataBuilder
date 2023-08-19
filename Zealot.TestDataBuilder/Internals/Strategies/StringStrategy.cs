dc6d7a5a1ccd1b337cb4f56c4cf00c10099b7778
Tugbay Atilla <tugbayatilla@gmail.com>
2023-08-19 13:09:49 +0200

    refactor: use StringBuilder to append strings in StringStrategy

--- Zealot.TestDataBuilder/Internals/Strategies/StringStrategy.cs
+++ Zealot.TestDataBuilder/Internals/Strategies/StringStrategy.cs
@@ -1,4 +1,5 @@
 ﻿using System.Linq.Expressions;
+using System.Text;
 using Zealot.Internals.Interfaces;
 
 namespace Zealot.Internals.Strategies;
@@ -6,15 +7,18 @@ namespace Zealot.Internals.Strategies;
 internal class StringStrategy : IStrategy
 {
     private int _number;
-    
-    public Expression<Func<Type, bool>> ResolveCondition 
-        => info => AvailableTypes.Any(x=>x == info);
+
+    public Expression<Func<Type, bool>> ResolveCondition => t => t == typeof(string);
 
     public object Execute(IContext context)
     {
-        _number++;
-        return $"{context.With.String.Prefix}{context.Scope.ParentPropertyName}_{_number}{context.With.String.Suffix}";
+        var sb = new StringBuilder();
+        sb.Append(context.With.String.Prefix);
+        sb.Append(context.Scope.ParentPropertyName);
+        sb.Append('_');
+        sb.Append(++_number);
+        sb.Append(context.With.String.Suffix);
+        
+        return sb.ToString();
     }
-    
-    private static IEnumerable<Type> AvailableTypes => new[] { typeof(string) };
 }
