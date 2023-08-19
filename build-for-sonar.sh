79047fc864e6f5c17aa7f06c1b97f25180ebfe39
Tugbay Atilla <tugbayatilla@gmail.com>
2023-08-19 14:01:04 +0200

    feat: add build script for sonarqube on local

---
+++ build-for-sonar.sh
@@ -0,0 +1,7 @@
+dotnet sonarscanner begin /k:"Zealot.TestDataBuilder" /d:sonar.token="$LOCAL_SONARQUBE_TOKEN" /d:sonar.host.url="http://localhost:9000"  /d:sonar.cs.dotcover.reportsPaths=dotCover.Output.html
+
+dotnet build –no-incremental
+
+dotnet dotcover test --dcReportType=HTML
+
+dotnet sonarscanner end /d:sonar.token="$LOCAL_SONARQUBE_TOKEN"
