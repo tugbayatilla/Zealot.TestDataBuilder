805e018d266548343b8b84374d99d8678d22f012
Tugbay Atilla <tugbayatilla@gmail.com>
2023-08-20 14:19:03 +0200

    feat: rename file and add remove command for some folders

--- build-for-local-sonar.sh
+++ build-for-local-sonar.sh
@@ -1,3 +1,6 @@
+rm dotCover.Output -rf
+rm .sonarqube -rf
+
 dotnet sonarscanner begin /k:"Zealot.TestDataBuilder" /d:sonar.token="$LOCAL_SONARQUBE_TOKEN" /d:sonar.host.url="http://localhost:9000"  /d:sonar.cs.dotcover.reportsPaths=dotCover.Output.html
 
 dotnet build –no-incremental
