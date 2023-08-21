8b52bc1e561a55c05b3f2363f31f75dc6fedd487
Tugbay Atilla <tugbayatilla@gmail.com>
2023-08-21 13:50:11 +0200

    fix: -rf location changed on rm command

--- build-for-local-sonar.sh
+++ build-for-local-sonar.sh
@@ -1,5 +1,7 @@
-rm dotCover.Output -rf
-rm .sonarqube -rf
+#!/bin/sh
+
+rm -rf ./dotCover.Output
+rm -rf ./.sonarqube
 
 dotnet sonarscanner begin /k:"Zealot.TestDataBuilder" /d:sonar.token="$LOCAL_SONARQUBE_TOKEN" /d:sonar.host.url="http://localhost:9000"  /d:sonar.cs.dotcover.reportsPaths=dotCover.Output.html
 
