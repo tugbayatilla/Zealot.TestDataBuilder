585d746b4e850c068e39bac20ec7bd1e8af500b0
Tugbay Atilla <tugbayatilla@gmail.com>
2023-09-01 16:45:01 +0200

    test: extend the test with unused properties

--- Zealot.TestDataBuilder.Tests/StructTests.cs
+++ Zealot.TestDataBuilder.Tests/StructTests.cs
@@ -1,3 +1,4 @@
+using FluentAssertions.Equivalency;
 using Zealot.Tests.TestObjects;
 
 namespace Zealot.Tests;
@@ -19,12 +20,38 @@ public class StructTests
     [Fact]
     public void Support_struct_directly()
     {
+        var a = Convert.ToByte('A');
         var subject = TestDataBuilder
             .For<StructWithAllPrimitives>()
             .Build();
 
         subject.Should().NotBeNull();
+        subject.BoolProp.Should().Be(true);
+        subject.BoolNullableProp.Should().Be(true);
         subject.IntProp.Should().Be(1);
+        subject.IntNullableProp.Should().Be(2);
+        subject.FloatProp.Should().Be(3);
+        subject.FloatNullableProp.Should().Be(4);
+        subject.DoubleProp.Should().Be(5);
+        subject.DoubleNullableProp.Should().Be(6);
+        subject.DecimalProp.Should().Be(7);
+        subject.DecimalNullableProp.Should().Be(8);
+        subject.LongProp.Should().Be(9);
+        subject.LongNullableProp.Should().Be(10);
+        subject.ShortProp.Should().Be(11);
+        subject.ShortNullableProp.Should().Be(12);
+        subject.ByteProp.Should().Be(a);
+        subject.ByteNullableProp.Should().Be(a);
+        subject.CharProp.Equals('A').Should().BeTrue();
+        subject.CharNullableProp!.Value.Equals('A').Should().BeTrue();
+        subject.StringProp.Should().MatchBuilderNamingRegex(nameof(subject.StringProp));
+        subject.StringNullableProp.Should().MatchBuilderNamingRegex(nameof(subject.StringNullableProp));
+        subject.UInt16Prop.Should().Be(13);
+        subject.UInt16NullableProp.Should().Be(14);
+        subject.UInt32Prop.Should().Be(15);
+        subject.UInt32NullableProp.Should().Be(16);
+        subject.UInt64Prop.Should().Be(17);
+        subject.UInt64NullableProp.Should().Be(18);
     }
     
     [Fact]
