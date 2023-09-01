22c791269ab098bd450c65e0741103b42b1b83e1
Tugbay Atilla <tugbayatilla@gmail.com>
2023-09-01 13:54:37 +0200

    test: support creating dictionary of object and object

--- Zealot.TestDataBuilder.Tests/GenericTests.cs
+++ Zealot.TestDataBuilder.Tests/GenericTests.cs
@@ -12,4 +12,15 @@ public class GenericTests
         subject.Should().NotBeNull();
         subject.Count.Should().Be(2);
     }
+    
+    [Fact]
+    public void Support_generic_dictionary_of_object_and_object()
+    {
+        var subject = TestDataBuilder
+            .For<Dictionary<object,object>>()
+            .Build();
+
+        subject.Should().NotBeNull();
+        subject.Count.Should().Be(2);
+    }
 }
