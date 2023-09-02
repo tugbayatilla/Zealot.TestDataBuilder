cd585e916444a5ae078dac67ab83df8a1e1ff5ed
Tugbay Atilla <tugbayatilla@gmail.com>
2023-09-02 02:52:11 +0200

    refactor: add comments as docs to the main interface ITestDataBuilder

--- Zealot.TestDataBuilder/ITestDataBuilder.cs
+++ Zealot.TestDataBuilder/ITestDataBuilder.cs
@@ -5,62 +5,69 @@ namespace Zealot;
 public interface ITestDataBuilder<out TEntity>
 {
     /// <summary>
-    /// Required
+    /// Required: returns the expected object
     /// </summary>
     TEntity Build();
     
     /// <summary>
-    /// Optional
+    /// Optional: overrides default value which is nothing
+    /// set only the given type of the property, ignore the rest
     /// </summary>
     ITestDataBuilder<TEntity> WithOnly<TProperty>();
     
     /// <summary>
-    /// Optional
+    /// Optional: overrides default value which is nothing
+    /// set only the given type of the property, ignore the rest
     /// </summary>
     ITestDataBuilder<TEntity> WithOnly(Type type); 
     
     /// <summary>
-    /// Optional
+    /// Optional: overrides the property with the given action
     /// </summary>
     ITestDataBuilder<TEntity> WithOverride(Action<TEntity> action);
 
     /// <summary>
-    /// Optional
+    /// Optional: overrides default value which is UtcNow
     /// </summary>
     ITestDataBuilder<TEntity> WithDate(DateTime dateTime);
 
     /// <summary>
-    /// Optional
+    /// Optional: overrides default value which is NewGuid
     /// </summary>
     ITestDataBuilder<TEntity> WithGuid(Guid guid);
     
     /// <summary>
-    /// Optional
+    /// Optional: overrides default value which is 0.
+    /// Determines how deep the recursion goes.
     /// </summary>
     ITestDataBuilder<TEntity> WithRecursionLevel(int recursionLevel);
     
     /// <summary>
-    /// Optional
+    /// Optional: overrides default value which is nothing
+    /// Adds the given string to the beginning of all string properties
     /// </summary>
     ITestDataBuilder<TEntity> WithStringPrefix(string prefix);
     
     /// <summary>
-    /// Optional
+    /// Optional: overrides default value which is nothing
+    /// Adds the given string to the end of all string properties
     /// </summary>
     ITestDataBuilder<TEntity> WithStringSuffix(string suffix);
     
     /// <summary>
-    /// Optional
+    /// Optional: overrides default value which is 1.
+    /// Uniqueness can be achieved with the incremental number.
     /// </summary>
     ITestDataBuilder<TEntity> WithStartingNumber(int startingNumber);
     
     /// <summary>
-    /// Optional
+    /// Optional: overrides default value which is NullLogger.Instance.
     /// </summary>
     ITestDataBuilder<TEntity> WithLogger(ILogger logger);
 
     /// <summary>
-    /// Optional
+    /// Optional: overrides default value which is 2.
+    /// Lists will have elements which are given as size.
     /// </summary>
     ITestDataBuilder<TEntity> WithListSize(int size);
 }
