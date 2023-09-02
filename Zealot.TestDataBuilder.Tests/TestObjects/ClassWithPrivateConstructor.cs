750971d9020e793f3bfa47c994b6af587f5216e7
Tugbay Atilla <tugbayatilla@gmail.com>
2023-09-02 02:05:28 +0200

    refactor: remove unused static init method for ClassWithPrivateConstructor

--- Zealot.TestDataBuilder.Tests/TestObjects/ClassWithPrivateConstructor.cs
+++ Zealot.TestDataBuilder.Tests/TestObjects/ClassWithPrivateConstructor.cs
@@ -2,7 +2,6 @@ namespace Zealot.Tests.TestObjects;
 
 internal abstract class ClassWithPrivateConstructor
 {
-    public static ClassWithPrivateConstructor Init() => null!;
     private ClassWithPrivateConstructor()
     {
     }
