9e42ff261bf64d750d3116817ba1510b55846ee3
Tugbay Atilla <tugbayatilla@gmail.com>
2023-09-02 02:04:15 +0200

    refactor: remove unused constructors for ClassWithNoProperty

--- Zealot.TestDataBuilder.Tests/TestObjects/ClassWithNoProperty.cs
+++ Zealot.TestDataBuilder.Tests/TestObjects/ClassWithNoProperty.cs
@@ -1,14 +1,3 @@
 namespace Zealot.Tests.TestObjects;
 
-internal class ClassWithNoProperty
-{
-    public ClassWithNoProperty()
-    {
-        
-    }
-
-    protected ClassWithNoProperty(int x)
-    {
-        
-    }
-}
+internal class ClassWithNoProperty { }
