6837afb9149cd0f26d4350441f534c574d7e8e70
Tugbay Atilla <tugbayatilla@gmail.com>
2023-09-02 10:28:26 +0200

    refactor: suppress message for ClassWithPrivateConstructor

--- Zealot.TestDataBuilder.Tests/TestObjects/ClassWithPrivateConstructor.cs
+++ Zealot.TestDataBuilder.Tests/TestObjects/ClassWithPrivateConstructor.cs
@@ -1,5 +1,8 @@
+using System.Diagnostics.CodeAnalysis;
+
 namespace Zealot.Tests.TestObjects;
 
+[SuppressMessage("Sonar", "S3453")]
 internal abstract class ClassWithPrivateConstructor
 {
     private ClassWithPrivateConstructor()
