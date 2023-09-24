f280fb7401dbffa298dd6c804de221f1e59a68c6
Tugbay Atilla <tugbayatilla@gmail.com>
2023-09-24 11:26:06 +0200

    feat: support with default on datetime

--- Zealot.TestDataBuilder.Tests/WithDefaultTests.cs
+++ Zealot.TestDataBuilder.Tests/WithDefaultTests.cs
@@ -83,4 +83,17 @@ public class WithDefaultTests
         instance.Prop1.Should().Be(default);
         instance.Prop2.Should().Be(default);
     }
+    
+    [Fact]
+    public void Support_Datetime()
+    {
+        var instance = TestDataBuilder
+            .For<ClassWithTwoDatetime>()
+            .WithDefault()
+            .Build();
+
+        instance.Should().NotBeNull();
+        instance.Prop1.Should().Be(default);
+        instance.Prop2.Should().Be(default);
+    }
 }
